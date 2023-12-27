3c3699e63c278adf4f13ba69cb9c3510bab589de
ahmet emin <62180556+miv403@users.noreply.github.com>
2023-12-27 23:23:21 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,49 @@
+# a python sikript for making a simple obsidian index file.
+# 2023-12-27
+# Author: miv403
+
+
+file = open("./list.txt", "r", encoding="utf-8")
+output = open("./output.txt", "w", encoding="utf-8")
+file = file.read()
+
+# reading a list from list.txt file then creating a output file.
+
+
+titleList = file.split("\n")
+
+# all file names should be seperated a newline character in list.txt file. 
+# i assuming the content of list.txt file is output of `ls` command that executed in a folder which contains only `*.md` files.
+
+length = len(titleList)
+# we will need index numbers of this list so we should keep the length of this list.
+
+replace = "" # temproary string variable for list item changes.
+for i in range(0,length):
+    # some file names may have single quote charater.
+    # `ls` is putting this character especially for file names which have blank character.
+    # so this character should be striped.
+    replace = titleList[i] 
+    replace = replace.strip("'")
+    titleList[i] = replace
+tmpList =[] # temproary list variable for list item changes
+for i in range(0,length):
+    # i am assuming that all file names have `.md` at the end.
+    # so this for loop converting each string item to list then
+    # removes the last three characters from it. the last three character is `.md`
+    replace = titleList[i] 
+    tmpList = list(replace)
+    tmpList = tmpList[0:-3]
+    replace = "".join(tmpList)
+    titleList[i] = replace
+
+for i in range(0,length):
+    # eventually we can build internal page link from this altered file names.
+    replace = titleList[i] 
+    replace = replace.strip()
+    replace = "[[" + replace + "]]" 
+    titleList[i] = replace
+
+for i in titleList:
+    # this is just writing the final list to output file.
+    output.write(i + "\n")
